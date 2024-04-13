b9694fa0169ff28efa3bc02f4fd1eb558e13d998
Monarca117 <skyggemonarken@gmail.com>
2024-04-13 15:01:39 -0600

    kusdsdfh

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 TELEGRAM_TOKEN = "6612690862:AAETYI_QEEbSGUvRhkr8o10ADmjrtuX05bM"
 
-ruta_reporte = "C:\\Users\\ruin0\\Documents\\TelegramBots\\ElectroBot\\Reporte\\Reporte.pdf"
+ruta_reporte = "Reporte\Reporte.pdf"
 
 ruta_recibo = "img\comoleerturecibodeluzcfe.jpg"
