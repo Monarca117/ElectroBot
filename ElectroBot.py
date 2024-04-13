1b820ea4e7b5e46c1a6d99bec95f36e53b14168f
Monarca117 <skyggemonarken@gmail.com>
2024-04-13 14:23:26 -0600

    Versión 1.0.0.0

--- ElectroBot.py
+++ ElectroBot.py
@@ -22,10 +22,10 @@ def cmd_start(message):
     bot.send_message(message.chat.id, "Puedes consultar nuestro aviso de privacidad aquí:", reply_markup=markup)
 
     print("ID del chat: ", message.chat.id)
-    bot.send_message(CHAT_ID, "Aqui tienes una lista de nuestros servicios: \nOpción 1. Consultar un reporte \nOpción 2. Consultar saldo y recibo \nOpción 3. Medios de pago \nOpción 4. Informacion")
+    bot.send_message(message.chat.id, "Aqui tienes una lista de nuestros servicios: \nOpción 1. Consultar un reporte \nOpción 2. Consultar saldo y recibo \nOpción 3. Medios de pago \nOpción 4. \n   \nSalir.")
 
 # Opciones válidas
-opciones_validas = ["opción 1", "opción 2", "opción 3", "opción 4"]
+opciones_validas = ["opción 1", "opción 2", "opción 3", "opción 4", "salir"]
 
 # Función para obtener la opción seleccionada
 def obtener_opcion(texto):
@@ -42,7 +42,7 @@ def handle_options(message):
 
     if opcion is None:
         bot.send_message(message.chat.id, "Opción no válida. Por favor, selecciona una opción de la lista.")
-        bot.send_message(message.chat.id, "Aqui tienes una lista de nuestros servicios: \nOpción 1. Consultar un reporte \nOpción 2. Consultar saldo y recibo \nOpción 3. Medios de pago \nOpción 4. Informacion")
+        bot.send_message(message.chat.id, "Aqui tienes una lista de nuestros servicios: \nOpción 1. Consultar un reporte \nOpción 2. Consultar saldo y recibo \nOpción 3. Medios de pago \nOpción 4. \nSalir.")
         return
 
     if opcion == "opción 1":
@@ -53,25 +53,33 @@ def handle_options(message):
 
     elif opcion == "opción 2":
         print("Opción 2 elegida")
-        bot.send_message(message.chat.id, "Has seleccionado la opción 2.")
+        user =  message.from_user
+        bot.send_message(message.chat.id, f"Usuario: {user.first_name} \nSaldo: 100")
+        bot.send_chat_action(message.chat.id, "Upload_document")
 
     elif opcion == "opción 3":
         print("Opción 3 elegida")
-        bot.send_message(message.chat.id, "Has seleccionado la opción 3.")
+        bot.send_message(message.chat.id, "Aquí te presento nuestros diferentes medios de pago: \n1.Pago en cajero. \n2. Pago con tarjeta de credito. \n 3.Pago con transferencia bancaria.")
 
     elif opcion == "opción 4":
         print("Opción 4 elegida")
-        bot.send_message(message.chat.id, "Has seleccionado la opción 4.")
+        bot.send_message(message.chat.id, "Para más información consulta nuestra página: [Electro Federation](http://www.Google.com)", parse_mode="MarkdownV2")
+
+    elif opcion == "salir":
+        print("Saliendo...")
+        user =  message.from_user
+        bot.send_message(message.chat.id, f"{user.first_name} gracias por habernos contactado. Nuestro compromiso es seguir conectados contigo.")
 
 # Manejador de ID de reporte
 def handle_report_id(message):
     id_reporte = message.text.lower()
     if id_reporte == "r01":
         print("Mandando reporte 1")
-        bot.send_message(message.chat.id, "Enviando reporte 1...")
+        bot.send_chat_action(message.chat.id, "Upload_document")
     elif id_reporte == "r02":
         print("Mandando reporte 2")
         bot.send_message(message.chat.id, "Enviando reporte 2...")
+        bot.send_chat_action(message.chat.id, "Upload_document")
     else:
         bot.send_message(message.chat.id, "ID de reporte no válido.")
 
