81c6145ee8c0c26ae8a886b47cc051760650f8ba
Monarca117 <136940920+Monarca117@users.noreply.github.com>
2024-04-13 14:59:31 -0600

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-
-TELEGRAM_TOKEN = "6612690862:AAETYI_QEEbSGUvRhkr8o10ADmjrtuX05bM"
-
-ruta_reporte = "C:\\Users\\ruin0\\Documents\\TelegramBots\\ElectroBot\\Reporte\\Reporte.pdf"
-
-ruta_recibo = "img\comoleerturecibodeluzcfe.jpg"
